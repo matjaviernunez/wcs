775994a788e26cc2a2dc3581dd75de797f908dbb
angelgaibor <mat.angel.gaibor@gmail.com>
2025-04-16 16:40:20 -0500

    cambios en el tratamiento de cites

--- 55_Entregable tabulados/rutinas/03_1_cites_tratamiento.R
+++ 55_Entregable tabulados/rutinas/03_1_cites_tratamiento.R
@@ -4,14 +4,53 @@ library(tidyverse)
 library(rio)
 library(openxlsx)
 
+#####
+# Año 2024
+
 cites24 <- import("02_Bases de Datos 2025/MAATE/BIODOVERSIDAD/CITES/Informe CITES 2024.xlsx",
                   sheet = "Exportación - Reexportación")
 
+aux24 <- cites24 |> 
+  mutate(descripcion = tolower(`Descripción del espécimen`),
+         cantidad = Cantidad,
+         unidades = case_when(Unidad %in% c("KGM", "MTQ", "NAR") ~ Unidad,
+                              Unidad == "Muestras" ~ "MUE",
+                              grepl("vivo", tolower(`Descripción del espécimen`)) ~ "NAR",
+                              T ~ "SIN"),
+         cod_pais_imp = case_when(`Pais de destino` == "COL" ~ "CO",
+                                  `Pais de destino` == "UK" ~ "GB",
+                                  !is.na(`Pais de destino`) ~ `Pais de destino`,
+                                  T ~ "--"),
+         tipo = case_when(grepl("vivo", descripcion) ~ "Vivo",
+                          grepl("completo", descripcion) ~ "Muerto",
+                          grepl("cuerpo", descripcion) ~ "Muerto",
+                          descripcion %in% c("especimen en etanol",
+                                             "especimen en formol",
+                                             "espécimen en etanol") ~ "Muerto",
+                          unidades %in% c("KGM", "MTQ", "MUE") ~ "Elemento",
+                          !is.na(descripcion) ~ "Elemento",
+                          T ~ "Sin tipo")) |> 
+  select(cod_permiso = `Número del permiso de exportación o del certificado de reexportación`,
+         apendice = `Apéndice`,
+         origen = Origen,
+         grupo = Grupo,
+         especie = Especie,
+         cod_comercio = `Codigo de comercio`,
+         descripcion,
+         proposito = `Propósito`,
+         anio = `Año`,
+         cantidad, 
+         unidades,
+         cod_pais_imp,
+         tipo)
+
+#####
+# Años 2022 y 2023
 
 cites22_23 <- read.xlsx("02_Bases de Datos 2025/MAATE/BIODOVERSIDAD/CITES/Matriz permisos CITES 2022 y 2023.xlsx",
                      sheet = 1, detectDates = T, fillMergedCells = T)
 
-cites22_23_01 <- cites22_23 |> 
+aux22_23 <- cites22_23 |> 
   mutate(
     cantidad = `#.de.especimenes` %>% str_extract('[:digit:]+'),
     auxiliar1 = `#.de.especimenes` %>%  str_extract('[:alpha:]+'),
@@ -37,38 +76,59 @@ cites22_23_01 <- cites22_23 |>
                      auxiliar1 %in% c("kg", "Kg", "kilo", "kilos") ~ "Elemento",
                      !is.na(descripcion) ~ "Elemento",
                      T ~ "Sin tipo"),
-    pais_imp = case_when(`Pais.de.importación` == "EEUU",
-                         `Pais.de.importación` == "Austria",            
-                         `Pais.de.importación` == "Perú",               
-                         `Pais.de.importación` == "España",            
-                         `Pais.de.importación` == "Holanda",            
-                         `Pais.de.importación` == "Hong Kong",          
-                         `Pais.de.importación` == "Alemania",           
-                         `Pais.de.importación` == "PL Doral FL33178",   
-                         `Pais.de.importación` == "Colombia",          
-                         `Pais.de.importación` == "Hong kong",          
-                         `Pais.de.importación` == "Ecuador",            
-                         `Pais.de.importación` == "Canada",             
-                         `Pais.de.importación` == "Dinamarca",          
-                         `Pais.de.importación` == "Chile",             
-                         `Pais.de.importación` == "Italia",             
-                         `Pais.de.importación` == "Republica checa",    
-                         `Pais.de.importación` == "Reino unido",        
-                         `Pais.de.importación` == "Singapur",           
-                         `Pais.de.importación` == "El Salvador",       
-                         `Pais.de.importación` == "Curazao"           
-                         `Pais.de.importación` == "Filpinas"           
-                         `Pais.de.importación` == "South Africa"       
-                         `Pais.de.importación` == "Taiwan"             
-                         `Pais.de.importación` == "Republica de korea"
-                         `Pais.de.importación` == "Peru"               
-                         `Pais.de.importación` == "Filipinas"          
-                         `Pais.de.importación` == "Korea"              
-                         `Pais.de.importación` == "portugal"           
-                         `Pais.de.importación` == "Australia"         
-                         `Pais.de.importación` == "Brasil"             
-                         `Pais.de.importación` == "Estados Unidos ")
-  )
+    cod_pais_imp = case_when(`Pais.de.importación` == "EEUU" ~ "US",
+                         `Pais.de.importación` == "Austria" ~ "AT",
+                         `Pais.de.importación` == "Perú" ~ "PE",
+                         `Pais.de.importación` == "España" ~ "ES",
+                         `Pais.de.importación` == "Holanda" ~ "NL",
+                         `Pais.de.importación` == "Hong Kong" ~ "HK",
+                         `Pais.de.importación` == "Alemania" ~ "DE",
+                         `Pais.de.importación` == "PL Doral FL33178" ~ "US",
+                         `Pais.de.importación` == "Colombia" ~ "CO",
+                         `Pais.de.importación` == "Hong kong" ~ "HK",
+                         `Pais.de.importación` == "Ecuador" ~ "EC",
+                         `Pais.de.importación` == "Canada" ~ "CA",
+                         `Pais.de.importación` == "Dinamarca" ~ "DK",
+                         `Pais.de.importación` == "Chile" ~ "CL",
+                         `Pais.de.importación` == "Italia" ~ "IT",
+                         `Pais.de.importación` == "Republica checa" ~ "CZ",
+                         `Pais.de.importación` == "Reino unido" ~ "GB",
+                         `Pais.de.importación` == "Singapur" ~ "SG",
+                         `Pais.de.importación` == "El Salvador" ~ "SV",
+                         `Pais.de.importación` == "Curazao" ~ "CW",
+                         `Pais.de.importación` == "Filpinas" ~ "PH",
+                         `Pais.de.importación` == "South Africa" ~ "ZA",
+                         `Pais.de.importación` == "Taiwan" ~ "TW",
+                         `Pais.de.importación` == "Republica de korea" ~ "KR",
+                         `Pais.de.importación` == "Peru" ~ "PE",
+                         `Pais.de.importación` == "Filipinas" ~ "PH",
+                         `Pais.de.importación` == "Korea" ~ "Korea",
+                         `Pais.de.importación` == "portugal" ~ "PT",
+                         `Pais.de.importación` == "Australia" ~ "AU",
+                         `Pais.de.importación` == "Brasil" ~ "BR",
+                         `Pais.de.importación` == "Estados Unidos " ~ "US",
+                         T ~ "--"),
+    anio = substr(as.character(Fecha), 1, 4),
+    apendice = "No declarado 22 23",
+    origen = "No declarado 22 23",
+    grupo = "En proceso",
+    cod_comercio = "No declarado 22 23"
+  ) |> 
+  select(cod_permiso = `No..Permiso.CITES`,
+         apendice,
+         origen,
+         grupo,
+         especie = Nombre.cientifico,
+         cod_comercio,
+         descripcion,
+         proposito = `Proposito`,
+         anio,
+         cantidad, 
+         unidades,
+         cod_pais_imp,
+         tipo)
 
-lol <- cites22_23_01 |> filter(is.na(cantidad))
-lol <- cites22_23_01 |> filter(unidades == "SIN")
+cites <- rbind(aux22_23, aux24)
+
+saveRDS(cites, "55_Entregable tabulados/intermedios/03_cites.rds")
+export(cites, "55_Entregable tabulados/intermedios/03_cites.xlsx")
